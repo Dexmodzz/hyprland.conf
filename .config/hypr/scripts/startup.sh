ccce19563012f380706d5a735daff35dc9dcb372
Nicola <nicola091998@gmail.com>
2025-07-24 06:57:55 +0200

    update scripts config.jsonc waybar  and install.sh

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-nm-applet
+#!/bin/bash
 
-sleep 0.5
+# Avvia dbus-session se non c'è
+if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+    eval $(dbus-launch --sh-syntax --exit-with-session)
+fi
 
-#other programs
+# Aspetta qualche secondo per sicurezza
+sleep 5
+
+# Lancia gli applet in background
+nm-applet &
+blueman-applet &
+
+if ! pgrep -x dbus-daemon > /dev/null; then
+    eval $(dbus-launch --sh-syntax)
+    echo "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
+fi
+
+
+nm-applet &
+
+
+sleep 1
+
+blueman-applet &
+
+
+wait
